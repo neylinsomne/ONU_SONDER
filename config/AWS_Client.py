7ab2fad7080ce3e7093e65a9a1f8bd95a9822aef
zaidpantoja <alberthzaid2022@hotmail.com>
2024-11-12 16:34:14 -0500

    Chat bot created

--- config/AWS_Client.py
+++ config/AWS_Client.py
@@ -7,7 +7,7 @@ def create_client(service:str):
     client = boto3.client(
 
     service, 
-    region_name=os.getenv("REGION"),  
+    region_name=os.getenv("REGION"), 
     aws_access_key_id=os.getenv("KEY_ID"),  
     aws_secret_access_key=os.getenv("KEY_ACCESS")
     )
